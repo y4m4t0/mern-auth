4769483f0eacc40c0055604dec9f19b5cb8faa09
y4m4t0 <argun63901@gmail.com>
2019-07-18 15:36:49 +0600

    Register validation added

---
+++ validation/register.js
@@ -0,0 +1,26 @@
+const Validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = function ValidateRegisterInput(data){
+    let errors = {};
+
+    data.name = isEmpty(data.name) ? data.name : "";
+    data.password = isEmpty(data.password) ? data.password : "";
+
+    if(Validator.isEmpty(data.name)){
+        errors.name = 'Name field is required';
+    }
+
+    if(Validator.isEmpty(data.password)){
+        errors.password = "Password is required";
+    }
+
+    if( !Validator.isLength(data.password, { min:6, max: 30 })){
+        errors.password = "Password should contain between 6 and 30 characters";
+    }
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
