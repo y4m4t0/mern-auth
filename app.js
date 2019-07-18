e73fe9e098efe5c8a031deb6fe441a51259a151f
y4m4t0 <argun63901@gmail.com>
2019-07-18 15:14:15 +0600

    Init commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// connection body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://localhost:27017/mern-auth')
+    .then(() => console.log(`MongoDB connected`));
+
+
+
+port = process.env.port || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
